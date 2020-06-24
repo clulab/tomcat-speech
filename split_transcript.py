715d1ae2fff9cc3deb33f1250146be069c70ef5e
meghavarshini <meghavarshini@gmail.com>
2020-06-24 12:51:00 -0700

    added comments to split_transcript.py

--- split_transcript.py
+++ split_transcript.py
@@ -3,13 +3,25 @@ import os
 import re
 import glob
 
+########
+# this code takes a filepath as argument, 
+# searches for all filenames that end with "video_transcript.txt"
+# and splits them at utterance level
+# NOTE: This code does not preserve sentence punctuation
+########
+
+
+
+# test if filepath given works
 if __name__ == "__main__":
 	if os.path.isdir(sys.argv[1]) == False:
 		sys.exit("ERROR: bad directory path")
 
-out = sys.argv[1] + "/output"
+# create an output folder:
+out = sys.argv[1] + "/out"
 os.system('if [ ! -d "{0}" ]; then mkdir -p {0}; fi'.format(out))
-### Variables ####
+
+### Variable ####
 loc = sys.argv[1]
 
 # list all relevant text files
@@ -18,6 +30,8 @@ def txt(location):
 	files = glob.glob(fold)
 	return(files)
 
+
+# split utterances by searching for end punctuation:
 def text(ls):
 	for item in ls:
 		nm = item.split(".txt")
@@ -31,9 +45,6 @@ def text(ls):
 			for m in sp:
 				file_handler.write("%s\n" % m)
 
-		# 
-		# 	for i in transcript:
-		# 		file_handler.write("{}\n".format(i))
 
 z = txt(loc)
 if len(z) > 0:
