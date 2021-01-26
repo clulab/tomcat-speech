f0e2dd0937992c0e72f588fe4cba218285702d72
Adarsh Pyarelal <adarsh@email.arizona.edu>
2021-01-26 16:26:31 -0700

    Adding entrypoint for vanga deployment

---
+++ agents/asr/entrypoints/vanga.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Entrypoint for deployment on vanga.sista.arizona.edu. We add extra options
+# for enabling https - we provide paths to the SSL certificate and key file
+# using environment variables.
+
+./tomcat_asr_agent websockets\
+  --ws_host 0.0.0.0 --ws_port 8888\
+  --ssl_cert_chain $SSL_CERT_CHAIN --ssl_keyfile $SSL_KEYFILE\
+    | mosquitto_pub -t agents/asr -l -h mosquitto
