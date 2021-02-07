1500a3d0c976f1baccf693bb2509d4ed19571857
Adarsh Pyarelal <adarsh.pyarelal@gmail.com>
2021-02-07 16:24:02 -0700

    Fixing bug where header timestamp was not incrementing (#39)

--- agents/asr/messages.py
+++ agents/asr/messages.py
@@ -2,16 +2,24 @@ import datetime
 from dataclasses import dataclass, field
 from typing import Optional
 
+
 @dataclass(frozen=True)
 class Header(object):
-    timestamp: str = field(default_factory=lambda: datetime.datetime.utcnow().isoformat() + "Z")
+    timestamp: str = field(init=False)
     message_type: str = "observation"
     version: str = "0.1"
+
+    def __post_init__(self):
+        object.__setattr__(
+            self, "timestamp", datetime.datetime.utcnow().isoformat() + "Z"
+        )
 
 
 @dataclass(frozen=True)
 class Msg(object):
-    timestamp: str = field(default_factory=lambda: datetime.datetime.utcnow().isoformat() + "Z")
+    timestamp: str = field(
+        default_factory=lambda: datetime.datetime.utcnow().isoformat() + "Z"
+    )
     experiment_id: Optional[str] = None
     participant_id: Optional[str] = None
     trial_id: Optional[str] = None
@@ -31,5 +39,8 @@ class Message(object):
     """Class to represent a testbed message."""
 
     data: Data
-    header: Header = Header()
-    msg: Msg = Msg()
+    msg: Msg
+    header: Header = field(init=False)
+
+    def __post_init__(self):
+        self.header = Header()
