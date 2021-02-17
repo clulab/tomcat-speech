1871fbd2c66ecc3d88af32e7d97a2bcb59242f70
Seongjin Park <seongjinpark@email.arizona.edu>
2021-02-16 19:49:20 -0700

    update wrong code

--- models/input_models_sjp.py
+++ models/input_models_sjp.py
@@ -436,7 +436,8 @@ class EarlyFusionMultimodalModel(nn.Module):
             else:
                 # print(acoustic_input.shape)
                 if len(acoustic_input.shape) > 2:
-                    elf.acoustic_fc_2 = nn.Linear(100, 20)  F.dropout(self.acoustic_fc_2(encoded_acoustic), self.dropout)
+                    self.acoustic_fc_2 = nn.Linear(100, 20)  
+                    encoded_acoustic = F.dropout(self.acoustic_fc_2(encoded_acoustic), self.dropout)
             )
             # print(encoded_acoustic.shape)
             # encoded_acoustic = self.acoustic_batch_norm(encoded_acoustic)
