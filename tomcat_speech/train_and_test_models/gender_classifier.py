be9811d5e343781f73fb24978f22ec419795d3a2
meghavarshini <meghavarshini@gmail.com>
2021-04-19 09:47:48 -0700

    changes for readability

--- tomcat_speech/train_and_test_models/gender_classifier.py
+++ tomcat_speech/train_and_test_models/gender_classifier.py
@@ -1,31 +1,1 @@
-import pandas as pd
-
-
-def genderclassifier(f0_list):
-    n = len(f0_list)
-    beg = int(round(0.25 * n))
-    end = int(round(0.75 * n))
-    f0 = pd.DataFrame(f0_list)
-
-    df = f0.iloc[
-        beg:end,
-    ]
-    val = df[(df["F0final_sma"] >= 50) & (df["F0final_sma"] <= 360)]
-    f0_val = mean(val["F0final_sma"])
-
-    model = pd.read_csv("Downloads/datasets/f0_list.csv")
-    f0_m = model[model["gender"] == 2].mean()
-    f0_f = model[model["gender"] == 1].mean()
-
-    diff1 = f0_val - f0_m["f0"]
-    diff2 = f0_val - f0_f["f0"]
-    gender_unknown = None
-    if diff1 <= 0:
-        gender_unknown = 2
-    elif diff2 >= 0:
-        gender_unknown = 1
-    elif abs(diff2) > abs(diff1):
-        gender_unknown = 2
-    elif abs(diff1) >= abs(diff2):
-        gender_unknown = 1
-    return gender_unknown
+import pandas as pddef genderclassifier(f0_list): #function that takes a list of pitch values as input calculates the mean pitch, and returns a gender value    n = len(f0_list)    beg = int(round(0.25 * n))    end = int(round(0.75 * n))    f0 = pd.DataFrame(f0_list)    # filter the list to remove the first 25% and last 25% values    df = f0.iloc[        beg:end,    ]    # remove outliers    val = df[(df["F0final_sma"] >= 50) & (df["F0final_sma"] <= 360)]    f0_val = mean(val["F0final_sma"])    # calculate mean pitch from existing dataset    #TO DO: find away to store these mean values in function    model = pd.read_csv("~/Downloads/datasets/f0_list.csv")    f0_m = model[model["gender"] == 2].mean()    f0_f = model[model["gender"] == 1].mean()    # classifer rules:    diff1 = f0_val - f0_m["f0"]    diff2 = f0_val - f0_f["f0"]    gender_unknown = None    if diff1 <= 0:        gender_unknown = 2    elif diff2 >= 0:        gender_unknown = 1    elif abs(diff2) > abs(diff1):        gender_unknown = 2    elif abs(diff1) >= abs(diff2):        gender_unknown = 1    return gender_unknown
