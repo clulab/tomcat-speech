cd60e88f804fe64aaf5e8017fa0ed7a22b4da426
john culnan <jmculnan@email.arizona.edu>
2022-01-21 10:46:34 -0700

    generic single-task training file added

--- tomcat_speech/models/plot_training.py
+++ tomcat_speech/models/plot_training.py
@@ -4,44 +4,84 @@ import matplotlib.pyplot as plt
 
 
 def plot_train_dev_curve(
-    train_losses,
-    dev_losses,
+    train_vals,
+    dev_vals,
     x_label="",
     y_label="",
     title="",
     save_name=None,
     show=False,
-    losses=True,
-    set_axis_boundaries=True,
+    axis_boundaries=None
 ):
     """
-    plot the loss or accuracy curves over time for training and dev set
+    plot the loss or accuracy/f1 curves over time for training and dev set
+    :param train_vals: a list of losses/f1/acc on train set
+    :param dev_vals: a list of losses/f1/acc on dev set
+    :param x_label: x-axis label
+    :param y_label: y-axis label
+    :param title: generic plot title; appended with loss or f1
+    :param save_name: the name used for saving the image
+    :param show: whether to show the image
+    :param axis_boundaries: either (lower_bound, upper_bound) tuple
+        or None, for automatic boundary setting
     """
     # get a list of the epochs
-    epoch = [i for i, item in enumerate(train_losses)]
+    epoch = [i for i, item in enumerate(train_vals)]
 
     # prepare figure
     fig, ax = plt.subplots()
     plt.grid(True)
 
     # add losses/epoch for train and dev set to plot
-    ax.plot(epoch, train_losses, label="train")
-    ax.plot(epoch, dev_losses, label="dev")
+    ax.plot(epoch, train_vals, label="train")
+    ax.plot(epoch, dev_vals, label="dev")
 
     # label axes
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
 
     # depending on type of input, set the y axis boundaries
-    if set_axis_boundaries:
-        if losses:
-            ax.set_ylim([0.66, 0.72])
-        else:
-            ax.set_ylim([0.3, 1.0])
+    if axis_boundaries:
+        ax.set_ylim(axis_boundaries[0], axis_boundaries[1])
 
     # create title and legend
     ax.set_title(title, loc="center", wrap=True)
     ax.legend()
+
+    # save the file
+    if save_name is not None:
+        plt.savefig(fname=save_name)
+        plt.close()
+
+    # show the plot
+    if show:
+        plt.show()
+
+
+def plot_histograms_of_data_classes(
+        data_list,
+        x_label="",
+        y_label="",
+        title="",
+        save_name="",
+        show=False
+):
+    """
+    Plot histograms for the number of items per class in the data
+    :param data_list: a list containing all gold labels for dataset
+    """
+    # prepare figure
+    fig, ax = plt.subplots()
+    plt.grid(True)
+
+    num_classes = set(data_list)
+
+    # add losses/epoch for train and dev set to plot
+    ax.hist(data_list, bins=[i for i in range(len(num_classes) + 1)]) #, bins=[0, .25 * total, .5 * total, .75 * total, 1 * total])
+
+    # label axes
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
 
     # save the file
     if save_name is not None:
---
+++ tomcat_speech/train_and_test_models/train_single_task.py
@@ -0,0 +1,219 @@
+# train a single-task model for any of the five tasks
+# combining the training of the separate train_task models into one
+
+import pickle
+import shutil
+import sys
+import os
+import torch
+import numpy as np
+from datetime import date
+import random
+
+from tomcat_speech.models.train_and_test_models import multitask_train_and_predict, make_train_state
+from tomcat_speech.models.input_models import MultitaskModel
+from tomcat_speech.models.plot_training import *
+
+# import parameters for model
+import tomcat_speech.models.parameters.singletask_config as config
+
+# import MultitaskObject and Glove from preprocessing code
+sys.path.append("../multimodal_data_preprocessing")
+from utils.data_prep_helpers import MultitaskObject, Glove, make_glove_dict
+
+# set cuda
+cuda = False
+if torch.cuda.is_available():
+    cuda = True
+
+device = torch.device("cuda" if cuda else "cpu")
+
+# # Check CUDA
+if torch.cuda.is_available():
+    torch.cuda.set_device(2)
+
+# set random seed
+torch.manual_seed(config.model_params.seed)
+np.random.seed(config.model_params.seed)
+random.seed(config.model_params.seed)
+if cuda:
+    torch.cuda.manual_seed_all(config.model_params.seed)
+
+if __name__ == "__main__":
+    # check if cuda
+    print(cuda)
+
+    if cuda:
+        # check which GPU used
+        print(torch.cuda.current_device())
+
+    # decide if you want to use avgd feats
+    avgd_acoustic_in_network = (
+        config.model_params.avgd_acoustic or config.model_params.add_avging
+    )
+
+    # create save location
+    output_path = os.path.join(
+        config.exp_save_path,
+        str(config.EXPERIMENT_ID)
+        + "_"
+        + config.EXPERIMENT_DESCRIPTION
+        + str(date.today()),
+    )
+
+    print(f"OUTPUT PATH:\n{output_path}")
+
+    # set location for pickled data (saving or loading)
+    if config.USE_SERVER:
+        data = "/data/nlp/corpora/MM/pickled_data"
+    else:
+        data = "data"
+
+    # make sure the full save path exists; if not, create it
+    os.system('if [ ! -d "{0}" ]; then mkdir -p {0}; fi'.format(output_path))
+
+    # copy the config file into the experiment directory
+    shutil.copyfile(config.CONFIG_FILE, os.path.join(output_path, "config.py"))
+
+    # add stdout to a log file
+    with open(os.path.join(output_path, "log"), "a") as f:
+        if not config.DEBUG:
+            sys.stdout = f
+
+            # 1. Load datasets + glove object
+            load_path = config.load_path
+            feature_set = config.feature_set
+
+            # import data
+            train_ds = pickle.load(open(f"{load_path}/{config.task}_{feature_set}_train.pickle", "rb"))
+            dev_ds = pickle.load(open(f"{load_path}/{config.task}_{feature_set}_dev.pickle", "rb"))
+            test_ds = pickle.load(open(f"{load_path}/{config.task}_{feature_set}_test.pickle", "rb"))
+            class_weights = pickle.load(open(f"{load_path}/{config.task}_{feature_set}_clsswts.pickle", "rb"))
+
+            # if not using distilbert embeddings
+            if not config.model_params.use_distilbert:
+                # make glove
+                glove_dict = make_glove_dict(config.glove_path)
+                glove = Glove(glove_dict)
+
+                # get set of pretrained embeddings and their shape
+                pretrained_embeddings = glove.data
+                num_embeddings = pretrained_embeddings.size()[0]
+                print(f"shape of pretrained embeddings is: {glove.data.size()}")
+
+            # 3. CREATE NN
+            print(config.model_params)
+
+            save_path = os.path.join(
+                output_path,
+                f"LR{config.model_params.lr}_BATCH{config.model_params.batch_size}_"
+                f"NUMLYR{config.model_params.num_gru_layers}_"
+                f"SHORTEMB{config.model_params.short_emb_dim}_"
+                f"INT-OUTPUT{config.model_params.output_dim}_"
+                f"DROPOUT{config.model_params.dropout}",
+            )
+
+            # make sure the full save path exists; if not, create it
+            os.system(f'if [ ! -d "{save_path}" ]; then mkdir -p {save_path}; fi')
+
+            # this uses train-dev-test folds
+            # create instance of model
+            if config.model_params.use_distilbert:
+                task_model = MultitaskModel(
+                    params=config.model_params,
+                    use_distilbert=config.model_params.use_distilbert
+                )
+            else:
+                task_model = MultitaskModel(
+                    params=config.model_params,
+                    use_distilbert=config.model_params.use_distilbert,
+                    num_embeddings=num_embeddings,
+                    pretrained_embeddings=pretrained_embeddings
+                )
+
+            optimizer = torch.optim.Adam(
+                lr=config.model_params.lr,
+                params=task_model.parameters(),
+                weight_decay=config.model_params.weight_decay,
+            )
+
+            # set the classifier(s) to the right device
+            task_model = task_model.to(device)
+            print(task_model)
+
+            # add loss function for cdc
+            loss_func = torch.nn.CrossEntropyLoss(
+                weight=class_weights.to(device),
+                reduction="mean"
+            )
+
+            # create multitask object
+            task_obj = MultitaskObject(
+                train_ds,
+                dev_ds,
+                test_ds,
+                loss_func,
+                task_num=0,
+            )
+
+            # set all data list
+            all_data_list = [
+                task_obj
+            ]
+
+            print(
+                "Model, loss function, and optimization created"
+            )
+
+            # todo: set data sampler?
+            sampler = None
+            # sampler = BatchSchedulerSampler()
+
+            # create a a save path and file for the model
+            model_save_file = f"{save_path}/{config.EXPERIMENT_DESCRIPTION}.pt"
+
+            # make the train state to keep track of model training/development
+            train_state = make_train_state(config.model_params.lr, model_save_file,
+                                           config.model_params.early_stopping_criterion)
+
+            # train the model and evaluate on development set
+            multitask_train_and_predict(
+                task_model,
+                train_state,
+                all_data_list,
+                config.model_params.batch_size,
+                config.model_params.num_epochs,
+                optimizer,
+                device,
+                scheduler=None,
+                sampler=sampler,
+                avgd_acoustic=avgd_acoustic_in_network,
+                use_speaker=config.model_params.use_speaker,
+                use_gender=config.model_params.use_gender,
+            )
+
+            # plot the loss and accuracy curves
+            # set plot titles
+            loss_title = f"Training and Dev loss for model {config.model_params.model} with lr {config.model_params.lr}"
+            loss_save = f"{save_path}/loss.png"
+
+            # plot the loss from model
+            plot_train_dev_curve(
+                train_state["train_loss"],
+                train_state["val_loss"],
+                x_label="Epoch",
+                y_label="Loss",
+                title=loss_title,
+                save_name=loss_save
+            )
+
+            # plot the avg f1 curves for each dataset
+            for item in train_state["tasks"]:
+                plot_train_dev_curve(
+                    train_state["train_avg_f1"][item],
+                    train_state["val_avg_f1"][item],
+                    x_label="Epoch",
+                    y_label="Weighted AVG F1",
+                    title=f"Average f-scores for task {item} for model {config.model_params.model} with lr {config.model_params.lr}",
+                    save_name=f"{save_path}/avg-f1_task-{item}.png",
+                )
